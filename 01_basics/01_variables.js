f7d3102b11aacc31549f49efb010de83fae4a3be
ABHISHEK ANAND <163181055+Abhishek4881@users.noreply.github.com>
2024-05-12 07:00:10 +0000

    learn variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId= 144553
+let accountEmail= "abhimememr"
+var accPsw="807"
+accountcity= "Jaipur"
+let accountstate;
+
+// accountId=2
+
+accountEmail = "hc@hc.com"
+accPsw = "21212121"
+accountcity = "Bengaluru"
+
+console.log(accountId);
+
+console.log([accountId,accountEmail,accPsw,accountcity,accountstate])
