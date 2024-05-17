034358767cee10340041a9dbd232c87fb56a845b
ABHISHEK ANAND <163181055+Abhishek4881@users.noreply.github.com>
2024-05-17 19:14:44 +0000

    dataTypeConversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+let number= 33
+
+let StringNumber= String(number)
+
+// console.log(typeof number)
+// console.log(typeof StringNumber)
+// console.log(StringNumber)
+
+let emptyString=""
+let string="Ram"
+
+let numberString=Number(emptyString)
+let numbString=Number(string)
+
+// console.log(typeof numberString)
+// console.log(typeof numbString)
+
+// console.log(numberString)
+// console.log(numbString)
+
+let yes=1
+let no=0
+
+let boolYes=Boolean(yes)
+let boolNo=Boolean(no)
+
+// console.log(typeof boolYes)
+// console.log(typeof boolNo)
+
+// console.log(boolYes)
+// console.log(boolNo)
+
+let kucchNHi=null
+
+let nullinNUMBERS=Number(kucchNHi)
+
+console.log(typeof kucchNHi)
+console.log(typeof nullinNUMBERS)
+
+
+console.log( kucchNHi)
+console.log( nullinNUMBERS)
+
+
+
